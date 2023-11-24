a724493ef4553737def6292f72aaefe1bb78959d
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2023-11-24 18:35:15 +0100

    Spellcheck Paysafecard.php

--- fr_FR/Paysafecard.php
+++ fr_FR/Paysafecard.php
@@ -5,7 +5,7 @@
     If you want to help you translate into your language, you can contact us via our support.
 */
 return [
-  'paysafecard.accept' => 'Le transfert accepté avec succès.',
+  'paysafecard.accept' => 'Transfert accepté avec succès.',
   'paysafecard.refuse' => 'Transfert refusé avec succès.',
   'paysafecard.change' => 'Valeur de la carte modifiée.',
   'paysafecard.btn' => 'Annuler',
@@ -15,7 +15,7 @@ return [
   'paysafecard.cancel' => 'Transfert de fonds annulé.',
   'paysafecard.admin.title' => 'Transfert Paysafecard',
   'paysafecard.admin.subtitle' => 'Gérez les virements Paysafecard vers Crédit.',
-  'paysafecard.admin.warning' => 'Une fois la confirmation de la carte la valeur sera directement ajoutée au compte. <br / > Le premier bouton permet d\'annuler le code, le second accept et le troisième refuse le code.',
+  'paysafecard.admin.warning' => 'Une fois la confirmation de la carte effectuée, la valeur sera directement ajoutée au compte. <br / > Le premier bouton permet d\'annuler le code, le second accepte le code et le troisième refuse le code.',
   'paysafecard.admin.check' => 'Vérifier une carte',
   'paysafecard.fees' => 'Frais paysafecard',
   'paysafecard.form.pin' => 'Code PIN',
