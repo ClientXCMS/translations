dfddd2a69a51c2a0afc6d858cc172e42dc73bd05
Hyperion <35610696+Hyperion60@users.noreply.github.com>
2023-11-24 18:29:35 +0100

    Spellcheck Fund.php

--- fr_FR/Fund.php
+++ fr_FR/Fund.php
@@ -5,15 +5,15 @@
     If you want to help you translate into your language, you can contact us via our support.
 */
 return [
-  'fund.admin.max_fund' => 'Fund maximum pouvant être ajouter',
-  'fund.admin.min_fund' => 'Fund minimum devant être ajouter',
-  'fund.admin.maxtransfer_fund' => 'Fund maximum pouvant être transférer sur un autre compte',
-  'fund.admin.mintransfer_fund' => 'Fund minimum pouvant être transférer sur un autre compte',
-  'fund.admin.enabletransfer' => 'Activer les transferts de fond entre utilisateurs',
+  'fund.admin.max_fund' => 'Fonds maximum pouvant être ajouté',
+  'fund.admin.min_fund' => 'Fonds minimum devant être ajouté',
+  'fund.admin.maxtransfer_fund' => 'Fonds maximum pouvant être transféré sur un autre compte',
+  'fund.admin.mintransfer_fund' => 'Fonds minimum pouvant être transféré sur un autre compte',
+  'fund.admin.enabletransfer' => 'Activer les transferts de fonds entre utilisateurs',
   'fund.history' => 'Historique',
   'fund.sendtouser' => 'Envoyer à un utilisateur',
   'fund.currentwallet' => 'Vous avez actuellement %wallet% sur votre compte.',
   'fund.sendtousersub' => 'Après avoir effectué le virement au destinataire, l\'argent sera mis en attente pendant 5 minutes, puis transféré sur le compte du destinataire.',
-  'fund.add' => 'Ajout de fond',
+  'fund.add' => 'Ajout de fonds',
   'fund.addmessage' => 'Vous allez recevoir <b>%money%</b> sur votre portefeuille virtuel après avoir payé <b>%val%</b>.',
 ];
